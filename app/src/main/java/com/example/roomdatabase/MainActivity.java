837d878613e21fbf28aaf71e6386b32188841fff
Md. Sabbir Ahmed <85029908+AbuNayemSabbir@users.noreply.github.com>
2022-08-21 12:04:25 +0600

    init

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -7,19 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText firstName,secondName;
+    private EditText firstName,secondName,userID;
     private Button save;
+    private TextView msgTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userID=findViewById(R.id.userId);
         firstName=findViewById(R.id.firstName);
         secondName=findViewById(R.id.secondName);
         save=findViewById(R.id.saveToDB);
+        msgTv=findViewById(R.id.showTV);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,9 +32,23 @@ public class MainActivity extends AppCompatActivity {
                 AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                         AppDatabase.class, "RoomDB").allowMainThreadQueries().build();
                 UserDao userDao = db.userDao();
-                userDao.insert(new User(2,firstName.getText().toString(),secondName.getText().toString()));
-                firstName.setText("");
-                secondName.setText("");
+                Boolean check=userDao.is_exist(Integer.parseInt(userID.getText().toString()));
+                if(check==false)
+                {
+                    userDao.insert(new User(Integer.parseInt(userID.getText().toString()),firstName.getText().toString(),secondName.getText().toString()));
+                    userID.setText("");
+                    firstName.setText("");
+                    secondName.setText("");
+                    msgTv.setText("Inserted Successfully");
+
+
+                }
+                else {
+                    userID.setText("");
+                    firstName.setText("");
+                    secondName.setText("");
+                    msgTv.setText("Record is already exist");
+                }
 
 
             }
--- app/src/main/java/com/example/roomdatabase/UserDao.java
+++ app/src/main/java/com/example/roomdatabase/UserDao.java
@@ -2,10 +2,14 @@ package com.example.roomdatabase;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 @Dao
 public interface UserDao {
     @Insert
     void insert(User users);
+
+    @Query("SELECT EXISTS(SELECT * FROM User WHERE uid=:userId)")
+    Boolean is_exist(int userId);
 
 }
